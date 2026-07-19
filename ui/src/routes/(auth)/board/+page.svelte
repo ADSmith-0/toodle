19ec90b0d6045539ae3c50797134981c5dadde90
ADSmith-0 <adam00199382818@gmail.com>
2026-07-19 23:52:44 +0100

    Added board route

---
+++ ui/src/routes/(auth)/board/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Board</title>
+</svelte:head>
+
+<h1>Board</h1>
--- ui/src/routes/login/+page.svelte
+++ ui/src/routes/login/+page.svelte
@@ -1,5 +1,30 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+  // FIX: remove
+  function mockCall(time: number): Promise<void> {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res();
+      }, time);
+    });
+  }
+
+  let loading = $state<boolean>(false);
+  async function login() {
+    loading = true;
+    await mockCall(1_000);
+    loading = false;
+    goto("/board", { replaceState: true });
+  }
+</script>
+
 <svelte:head>
-	<title>Login</title>
+  <title>Login</title>
 </svelte:head>
 
 <h1>Login</h1>
+{#if loading}
+  <p>Loading...</p>
+{/if}
+<button onclick={login}>Login</button>
